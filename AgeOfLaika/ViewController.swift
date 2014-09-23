d26a52317edef4cd681097d3dca522888eae6617
Stephen Chinnadorai <schinnadorai@gmail.com>
2014-09-23 14:28:26 +0100

    First commit

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    @IBOutlet weak var dogYearsConvertedLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        
+        // let conversionConstant = 7
+        dogYearsConvertedLabel.hidden = false
+        dogYearsConvertedLabel.text = "\(humanYearsTextField.text.toInt()! * 7)" + " in dog years"
+    }
+    
 
 
 }
